19596709653a1542716ac69461a4aca1fc958ef0
DebabrataBanik <debabratabanik02@gmail.com>
2026-06-05 18:02:42 +0530

    refactor: add JWT token generator util
    - remove hashPassword util

---
+++ server/src/utils/generateToken.ts
@@ -0,0 +1,12 @@
+import { SignJWT } from "jose";
+
+export async function generateToken(userId: string){
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+  const token = await new SignJWT({ sub: userId })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('5m')
+    .sign(secret);
+
+  return token
+}
--- server/src/utils/hashPassword.ts
+++
@@ -1,5 +0,0 @@
-import bcrypt from "bcryptjs"
-
-export async function hashPassword(pw: string){
-  return bcrypt.hash(pw, 10)
-}
