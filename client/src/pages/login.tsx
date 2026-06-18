168fa2e5ec747ed46b7d207070f790dccb6c0e5a
DebabrataBanik <debabratabanik02@gmail.com>
2026-06-18 15:05:16 +0530

    feat: add login route page

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,146 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+type FormError = Partial<FormData>;
+
+export default function Login() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+  const [formError, setFormError] = useState<FormError>({});
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [message]);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    setFormError((prevError) => ({
+      ...prevError,
+      [name]: null,
+    }));
+  }
+
+  async function login(creds: FormData) {
+    try {
+      const res = await fetch("http://localhost:8000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(creds),
+      });
+      if (!res.ok) {
+        const error = await res.json();
+        throw Error(error.message || `${res.status}, ${res.statusText}`);
+      }
+      navigate("/", { replace: true });
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Error) setMessage(error.message);
+    }
+  }
+
+  async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const error: FormError = {};
+    const { email, password } = formData;
+    if (!email.trim() || !emailRegex.test(email.trim())) {
+      error.email = "Please enter a valid email address";
+    }
+    if (password.length < 4) {
+      error.password = "Please enter strong password";
+    }
+    setFormError(error);
+
+    if (Object.values(error).length === 0) {
+      await login({ email: email.trim(), password });
+      setFormData({
+        email: "",
+        password: "",
+      });
+    }
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="p-4">
+        <h1 className="text-xl font-semibold tracking-wide text-center leading-none">
+          Welcome to Jobtrace!
+        </h1>
+        <p className="text-text-tertiary text-center text-sm">
+          Manage your job applications
+        </p>
+
+        <p className="text-sm text-error text-center mt-4 h-5">
+          {message && message}
+        </p>
+
+        <form
+          noValidate
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-2 mt-2"
+        >
+          <label>
+            <span className="pl-1 text-sm">Email Address</span>
+            <input
+              name="email"
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="login-input"
+            />
+            <span className="login-error">{formError && formError.email}</span>
+          </label>
+          <label>
+            <span className="pl-1 text-sm">Password</span>
+            <input
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={handleChange}
+              minLength={4}
+              className="login-input"
+            />
+            <span className="login-error">
+              {formError && formError.password}
+            </span>
+          </label>
+          <button
+            className="w-full bg-accent p-1.5 rounded-sm mt-5 text-bg-primary focus:outline-accent focus:outline-offset-2"
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+        <p className="mt-10 text-center text-sm">
+          Don't have an account?
+          <button className="px-1 font-medium hover:underline">
+            Register Now
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+}
