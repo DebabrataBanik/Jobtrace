2e43ef7d75714ea77476545d9e49f8865fbaac10
DebabrataBanik <debabratabanik02@gmail.com>
2026-06-20 13:05:39 +0530

    feat: render user detail form loader data

--- client/src/components/UserItem.tsx
+++ client/src/components/UserItem.tsx
@@ -1,13 +1,19 @@
 import { ChevronRightIcon } from "lucide-react";
+import { useLoaderData } from "react-router";
 
 export default function UserItem() {
+  const user = useLoaderData();
   return (
     <div className="border-t border-t-border pt-3 mt-auto">
       <button className="group w-full flex items-center p-2 gap-2 hover:bg-accent-subtle rounded-sm duration-300">
         <div className="w-8 h-8 rounded-full bg-accent"></div>
         <div className="text-xs text-left">
-          <p className="font-medium">Johnny Dough</p>
-          <span>johnny@dough.com</span>
+          <p className="font-medium line-clamp-1">
+            {user ? user.name : "Johnny Dough"}
+          </p>
+          <span className="line-clamp-1">
+            {user ? user.email : "johnny@dough.com"}
+          </span>
         </div>
         <ChevronRightIcon
           size={15}
