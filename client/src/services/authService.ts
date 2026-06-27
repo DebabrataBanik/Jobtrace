ef3d57d924efe746e4f7498736e0481c2ab83140
DebabrataBanik <debabratabanik02@gmail.com>
2026-06-27 12:28:58 +0530

    refactor(client): return null on 401 & throw on errors in authService

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -4,10 +4,11 @@ export async function getUser() {
   const res = await fetch(`${BASE_URL}/auth/me`, {
     credentials: "include",
   });
-  if (res.status == 401) {
-    return false;
+  if (res.status == 401) return null;
+
+  if (!res.ok) {
+    throw new Error(`Server error: ${res.status}`);
   }
-  if (res.ok) {
-    return res.json();
-  }
+
+  return res.json();
 }
