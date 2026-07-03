4486b50aa389a5f46c23db563c2305e5ae0e7472
DebabrataBanik <debabratabanik02@gmail.com>
2026-07-03 16:09:37 +0530

    feat: add home nav to error boundary
    - updated error display to show error status and data instead of statusText.

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,18 +1,35 @@
-import { isRouteErrorResponse, useRouteError } from "react-router";
+import { isRouteErrorResponse, useNavigate, useRouteError } from "react-router";
 
 export default function ErrorBoundary() {
   const error = useRouteError();
+  const navigate = useNavigate();
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div className="text-center pt-10 font-medium">
-        {error.status} {error.statusText}
+      <div className="text-center pt-10 font-medium flex flex-col items-center gap-2">
+        {error.status} - {error.data}
+        <button
+          onClick={() => navigate("/", { replace: true })}
+          className="hover:underline"
+        >
+          Go Home
+        </button>
       </div>
     );
   }
 
   if (error instanceof Error) {
-    return <div className="text-center pt-10 font-medium">{error.message}</div>;
+    return (
+      <div className="text-center pt-10 font-medium flex flex-col items-center gap-2">
+        {error.message}
+        <button
+          onClick={() => navigate("/", { replace: true })}
+          className="hover:underline"
+        >
+          Go Home
+        </button>
+      </div>
+    );
   }
 
   return (
