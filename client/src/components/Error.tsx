cf4e9341f492054175226a2c6043abb36945507a
DebabrataBanik <debabratabanik02@gmail.com>
2026-07-04 22:32:32 +0530

    fix(router): catch TypeError to display user friendly UI
    - increase error UI text size

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -6,7 +6,7 @@ export default function ErrorBoundary() {
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div className="text-center pt-10 font-medium flex flex-col items-center gap-2">
+      <div className="text-center pt-10 font-medium flex flex-col items-center gap-2 text-lg">
         {error.status} - {error.data}
         <button
           onClick={() => navigate("/", { replace: true })}
@@ -19,8 +19,26 @@ export default function ErrorBoundary() {
   }
 
   if (error instanceof Error) {
+    if (error.name === "TypeError") {
+      return (
+        <div className="text-center pt-10 font-medium flex flex-col items-center gap-2">
+          <p className="text-lg">
+            Couldn't connect to server.
+            <span className="block">
+              Please check your internet connection and try again later.
+            </span>
+          </p>
+          <button
+            onClick={() => window.location.reload()}
+            className="hover:underline"
+          >
+            Retry
+          </button>
+        </div>
+      );
+    }
     return (
-      <div className="text-center pt-10 font-medium flex flex-col items-center gap-2">
+      <div className="text-center pt-10 font-medium flex flex-col items-center gap-2 text-lg">
         {error.message}
         <button
           onClick={() => navigate("/", { replace: true })}
